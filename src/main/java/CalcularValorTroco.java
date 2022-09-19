0b11760b010b644540061a796942c0f5b1ebbe61
Jonathas Santos <jonathaspe@hotmail.com>
2022-09-18 22:54:06 -0300

    Conclusão da classe de Cálculo de Troco

--- src/main/java/CalcularValorTroco.java
+++ src/main/java/CalcularValorTroco.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class CalcularValorTroco {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o valor do produto: ");
+        Double valorProduto = scanner.nextDouble();
+
+        System.out.print("Digite a quantidade passada pelo cliente: ");
+        Double valorPassadoPeloCliente = scanner.nextDouble();
+
+        Double resultado = valorPassadoPeloCliente - valorProduto;
+
+        System.out.println("Troco: " + resultado);
     }
 }
